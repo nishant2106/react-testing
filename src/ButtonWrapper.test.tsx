3f12b3ce8c5d6ed77588b81f5aaa775d354821de
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2023-02-19 15:22:25 +0530

    testing on event onClick

---
+++ src/ButtonWrapper.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import ButtonWrapper from "./ButtonWrapper";
+
+test("handles onClick", () => {
+  const onClick = jest.fn();
+  render(<ButtonWrapper title={"Add Item"} onClick={onClick} />);
+  const buttonElement = screen.getByText("Add Item");
+  fireEvent.click(buttonElement);
+  expect(onClick).toHaveBeenCalledTimes(1);
+});
---
+++ src/ButtonWrapper.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ButtonWrapper: React.FC<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > & { title: string }
+> = ({ title, ...props }) => {
+  return <button {...props}>{title}</button>;
+};
+
+export default ButtonWrapper;
