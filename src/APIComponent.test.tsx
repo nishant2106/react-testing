ddee3af93e16e24801d03d1667f4ac6bf33e4a4d
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2023-02-21 23:03:05 +0530

    test on api call and with useEffect

---
+++ src/APIComponent.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import APIComponent from "./APIComponent";
+
+const server = setupServer(
+  rest.get("/api", (req, res, ctx) => {
+    return res(ctx.json({ name: "Nishant" }));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test("gets the data", async () => {
+  render(<APIComponent />);
+
+  const out = await waitFor(() => screen.findByRole("contentinfo"));
+
+  expect(out).toHaveTextContent("Name is Nishant");
+});
